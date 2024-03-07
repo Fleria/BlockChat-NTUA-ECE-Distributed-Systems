323fdfb9f00afb3d6afc76007bbd0f65dda725e2
DimitrisAndreadis <61821098+DimitrisAndreadis@users.noreply.github.com>
2024-03-07 14:33:07 +0200

    Update node.py

--- source/node.py
+++ source/node.py
@@ -6,14 +6,27 @@ import blockchain
 class node:
     def _init_():
         self.BCC=100
+       
         #self.chain
         #self.current_id_count
         #self.NBCs
         #self.wallet
-
+        self.ring = []
         #self.ring[] #here we store information for every node(id, adrdress (ip:port), public key, balance)
 
     def create_new_block():
+
+    def register_node_to_ring(self, id, ip, port, public_key, balance):
+        """Registers a new node in the ring, called only by the bootstrap node"""
+
+        self.ring.append(
+            {
+                'id': id,
+                'ip': ip,
+                'port': port,
+                'public_key': public_key,
+                'balance': balance
+            })
 
     def generate_wallet(): #create a wallet for this node, with a public key and a private key
 
